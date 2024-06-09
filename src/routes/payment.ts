f46124682c410eee16d4914d57a91d5143d38061
Parth Pandya <211290107020p@gmit.edu.in>
2024-06-09 23:53:08 +0530

    test all the apis in postman

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -5,13 +5,13 @@ import { adminOnly } from "../middlewares/adminOnly.js";
 const router = Router();
 
 //route- /api/v1/payment/...
+//test all the apis in postman
 
 router.get("/discount", applyDiscount);
 
 router.post("/coupon/new",adminOnly ,newCoupon);
 router.get("/discount/all",adminOnly,allCoupons);
 router.delete("/coupon/:id",adminOnly,deleteCoupon);
-
 
 
 export default router;
